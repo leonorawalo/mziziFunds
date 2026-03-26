1c9ffed6355b4c0439d7e88b5f62ef7271080d41
leonorawalo <awaloleonora@gmail.com>
2026-03-26 13:05:26 +0300

    Auth dependencies adding to mix.exs... comeonin

--- mix.exs
+++ mix.exs
@@ -65,7 +65,10 @@ defmodule Mzizifunds.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:comeonin, "~> 5.3"},
+      {:bcrypt_elixir, "~> 3.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
